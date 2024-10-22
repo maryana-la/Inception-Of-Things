d3d4e6337fd71dff1f5a7e36ecede6b5586523b7
Olga Kushnirova <olgakushnirova27@gmail.com>
2024-10-22 15:56:37 +0200

    3rd part starting

---
+++ p3/install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+if ! command -v docker &> /dev/null
+then
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
+    sudo usermod -aG docker $USER
+    echo "Docker installed. Please restart the terminal."
+else
+    echo "Docker is already installed."
+fi
+
+if ! command -v k3d &> /dev/null
+then
+    curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+else
+    echo "K3D is already installed."
+fi
+
+if ! command -v kubectl &> /dev/null
+then
+    curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+    chmod +x ./kubectl
+    sudo mv ./kubectl /usr/local/bin/
+else
+    echo "kubectl is already installed."
+fi
+
+echo "Installation completed."
